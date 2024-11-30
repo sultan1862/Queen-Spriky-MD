0117c1aefd687eab8e39ed51d005158ef1d0e70d
sultan1862 <sultandon419@gmail.com>
2024-11-30 08:35:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || '6upwDJCa#FTv9qEp2RUAni9gej7dtred0vCq9cH8DnccI7ONaD2I', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'jatoi',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '923235306919',    // Enter Your Owner Number
 };
